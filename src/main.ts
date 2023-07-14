9e2a5bc9539e30eca5658f640b22ce17cb01eb38
manishjes <manishj@zignuts.com>
2023-07-14 19:56:44 +0530

    test

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,28 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  await app.listen(3001);
 }
 bootstrap();
+
+
+
+// import { NestFactory } from '@nestjs/core';
+// import { AppModule } from './app.module';
+// import { CorsOptions } from '@nestjs/common/interfaces/external/cors-options.interface';
+
+// async function bootstrap() {
+//   const app = await NestFactory.create(AppModule);
+
+//   // Enable CORS
+//   const corsOptions: CorsOptions = {
+//     origin: 'http://localhost:3000',
+//     methods: ['GET', 'POST', 'PUT', 'DELETE'],
+//     allowedHeaders: ['Content-Type'],
+//   };
+//   app.enableCors(corsOptions);
+
+//   await app.listen(3001);
+// }
+
+// bootstrap();
